4b98e80b4b7c13623e7cf67edd53dcc42d305c45
ssxdtxr <tereskinegor68@gmail.com>
2024-08-07 20:38:26 +0300

    Initial Commit

---
+++ budget-tracker/budget_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  budget_trackerApp.swift
+//  budget-tracker
+//
+//  Created by Егор Терёшкин on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct budget_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
